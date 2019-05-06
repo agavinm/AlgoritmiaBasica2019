0ba9eefc7999edeb940e4a8edf50d70e91cd7919
AndrewKM210 <andrewkm210@gmail.com>
2019-05-06 22:06:08 +0200

    Codifica bien, falta optimizar y decodificar

--- practica1/main.cpp
+++ practica1/main.cpp
@@ -29,15 +29,16 @@ void contar(const char* nombreFichero, unsigned char (&frecuencia)[N]) {
     unsigned char caracter;
     ifstream fichero(nombreFichero);
 
+    for (int i=0; i<N; i++) {
+        frecuencia[i] = 0;
+    }
+
     // Se cuenta el numero de apariciones de cada caracter leido del fichero.
     while (fichero.good()) {
         caracter = fichero.get();
         frecuencia[(int) caracter]++;
-        cout << "Encontrado caracter: " << caracter << endl;
     }
 }
-
-
 
 /*
  * Devuelve un heap en la que se han insertado los datos del vector 
@@ -49,8 +50,6 @@ vector<Arbol*> insertarFrecuencias(unsigned char frecuencia[N]) {
     // Por cada elemento del vector, se crea un arbol con la frecuencia del 
     // elemento y el elemento en si, y lo inserta al heap "h"..
     for (int i=0; i<=N; i++) {
-        cout << "Insertando frecuencia del caracter " << (unsigned char) i
-             << ": " << (int) frecuencia[i] << endl;
         Arbol* a = new Arbol(frecuencia[i], i);
         v.push_back(a);
     }
@@ -75,8 +74,6 @@ void crearTablaCodigos(Arbol *res, string (&codigos)[N], string codigo) {
 void escribirTablaCodigos(string nombreFichero, string (codigos)[N]) {
     ofstream tablaCodigos;
     tablaCodigos.open(nombreFichero);
-    cout << codigos[N] << endl;
-
 
     for (int i=0; i<= N; i++) {
         tablaCodigos << codigos[i] << endl;
@@ -101,17 +98,37 @@ void mostrarTablaCodigos(string (codigos)[N]) {
     }
 }
 
+
 void codificarTexto(string (codigos)[N], string ficheroEntrada, string ficheroSalida) {
-    unsigned char caracter;
+    unsigned char caracter, binario;
     ifstream ficheroOriginal(ficheroEntrada);
     ofstream ficheroCodificado;
     ficheroCodificado.open(ficheroSalida, ios::binary);
-    while (ficheroOriginal.good()) {
+    string codigo;
+    int i=0, tamanyo = sizeof(unsigned char)*8, j=0, k=0;
+    caracter = ficheroOriginal.get();
+    while (!ficheroOriginal.eof()) {
+        codigo = codigos[caracter];
+        while (i < codigo.size()) {
+            if (codigo[i] == '1') {
+                binario |= 1;
+            } else {
+                binario &= 0;
+            }
+            binario <<= 1;
+            if (j%tamanyo == tamanyo-1) {
+                ficheroCodificado.write(reinterpret_cast<const char*>(&binario), 1);
+                j = 0;
+            }
+            j++;
+            i++;
+        }
         caracter = ficheroOriginal.get();
-        // ESTAN ALMACENADOS CARACTERES, POR LO QUE SE ESCRIBEN CARACTERES Y
-        // OCUPA MUCHO ESPACIO. AVERIGUAR COMO HACERLO CON BINARIO.
-        ficheroCodificado.write(codigos[caracter].c_str(), codigos[caracter].size());
+        i = 0;
     }
+    ficheroCodificado.write(reinterpret_cast<const char*>(&binario), 1);
+    ficheroCodificado.close();
+    ficheroOriginal.close();
 }
 
 void decodificarTexto(string (codigos)[N], string ficheroEntrada, string ficheroSalida) {
@@ -126,41 +143,6 @@ void decodificarTexto(string (codigos)[N], string ficheroEntrada, string fichero
 
 int main() {
 
-    /*unsigned char frecuencia[N] = {0};
-    contar("prueba1.txt", frecuencia);
-    Heap h = insertarFrecuencias(frecuencia);*/
-
-
-    /*Heap h;
-    Arbol a1(10, 1), a2(20, 2), a3(30, 3), a4(40, 4), a5(50, 5), a6(60, 6), 
-          a7(70, 7), a8(80, 8), a9(90, 9), a0(0, 0);
-    Arbol a12(a1, a2), a56(a5, a6), a1256(a12, a56), a12560(a1256, a0);
-
-    h.insertar(a12560); h.insertar(a56); h.insertar(a12);
-    h.insertar(a1); h.insertar(a2); h.insertar(a3); h.insertar(a4); 
-    h.insertar(a5); h.insertar(a6); h.insertar(a7);
-    h.insertar(a8); h.insertar(a9); h.insertar(a0);
-    // Insertar ok, todos son montículos de mínimos
-
-    // Primero ok, devuelve el primero y reajusta el montículo
-    Arbol *aux = h.primero();
-
-    while (aux != nullptr) {
-        cout << (int)aux->frecuencia() << endl;
-        aux = h.primero();
-    }
-    
-    vector<Arbol*> v;
-
-    Arbol f(5, 'f'), e(9, 'e'), c(12, 'c'), b(13, 'b'), d(16, 'd'), a(45, 'a');
-    bool hoja = f.esHoja();
-    v.push_back(&e);
-    v.push_back(&f);
-    v.push_back(&a);
-    v.push_back(&c);
-    v.push_back(&d);
-    v.push_back(&b);
-    */
     unsigned char frecuencia[N] = {0};
     contar("prueba1.txt", frecuencia);
     vector<Arbol*> v = insertarFrecuencias(frecuencia);
@@ -169,10 +151,10 @@ int main() {
     crearTablaCodigos(res, codigos, "");
     mostrarTablaCodigos(codigos);
     escribirTablaCodigos("tablaCodigos.txt", codigos);
-    codificarTexto(codigos, "prueba1.txt", "prueba1_codificado.txt");
+    codificarTexto(codigos, "prueba1.txt", "prueba1Cod");
     string codigosRecuperados[N];
-    recuperarTablaCodigos("tablaCodigos.txt", codigosRecuperados);
-    mostrarTablaCodigos(codigosRecuperados);
+    //recuperarTablaCodigos("tablaCodigos.txt", codigosRecuperados);
+    //mostrarTablaCodigos(codigosRecuperados);
 
     return 0;
 }
